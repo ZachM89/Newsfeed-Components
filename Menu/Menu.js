b7150e97170e8058725a29fe44dcd5645e13a952
Zachary Murray <89foxlx@gmail.com>
2019-10-10 20:21:04 -0500

    completed menu.js and mvp requirements

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,90 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+
+function menuCreate(menuList){
+    let divElement = document.createElement('div');
+    let ulElement = document.createElement('ul');
+    let menuSelector = document.querySelector('.menu-button');
+    divElement.classList.add('menu');
+    for(let a = 0; a < menuList.length; a++){
+        let liElement = document.createElement('li');
+        liElement.textContent = menuList[a];
+         ulElement.appendChild(liElement);
+    }
+    
+    //let divMenu = document.querySelector('.menu');
+    menuSelector.addEventListener('click', (e) => {
+        divElement.classList.toggle('menu--open');
+    })
+    divElement.appendChild(ulElement);
+
+    return divElement;
+}
+
+let menuSelector = document.querySelector('.header');
+
+// menuSelector.addEventListener('click', (e) => {
+//     e.target.classList.toggle('menu--open');
+// })
+const allTheMenu = menuCreate(menuItems);
+menuSelector.appendChild(allTheMenu);
+
+// let divMenu = document.querySelector('.menu');
+// divMenu.addEventListener('click', (e) => {
+//     e.target.classList.toggle('menu--open');
+// })
+
+// function menuCreate(menuList){
+//     let divElement = document.createElement('div');
+//     let ulElement = document.createElement('ul');
+//     let menuSelector = document.querySelector('.menu-button');
+
+//     divElement.classList.add('menu');
+//     divElement.appendChild(ulElement);
+
+//     let menuStuff = [];
+
+//     menuStuff = menuList.map(
+//         (item) => {
+//             let liElement = document.createElement('li');
+//             liElement.textContent = item[a];
+//             return liElement;
+//         }
+//     )
+
+//     for(let a = 0; a < menuStuff.length; a++){
+//         ulElement.appendChild(menuStuff[a]);
+//     }
+//     // for(let a = 0; a < menuItems.length; a++){
+//     //     const liElement = menuItems[a];
+//     // }
+
+//     menuSelector.addEventListener('click', (e) => {
+//         e.target.classList.toggle('menu--open');
+//     })
+//     return
+// }
+/////////////////////////////////////////////////////
+// let theMenuList = [];
+
+// theMenuList = menuItems.map(
+//     (item) => {
+//         //let aComponent = componentCreate(item.title,item.date,item.firstParagraph,item.secondParagraph,item.thirdParagraph);
+//         //return aComponent;
+
+//         return menuCreate(item.title,item.date,item.firstParagraph,item.secondParagraph,item.thirdParagraph);
+//     }
+// )
+
+// const theMenu = document.querySelector('.menu');
+
+// for(let a = 0; a < menuItems.length; a++){
+//     theMenu.appendChild(menuItems[a]);
+// }
+////////////////////////////////////////////////////////
+//theMenu.appendChild(menuCreate(menuItems));
+
+
+
